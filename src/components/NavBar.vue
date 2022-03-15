0983a357914464933c28ee43df32220e25bddabc
Rémi Robichet <remi.robichet@edtake.com>
2022-03-15 17:07:25 +0100

    wip add google

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -30,7 +30,7 @@ export default {
   props: ['user'],
   data () {
     return {
-      providers: ['twitter', 'github', 'aad'],
+      providers: ['twitter', 'github', 'aad', 'google'],
       redirect: `/`
     }
   }
