20c48ed9e33bf0521161fdd450b9bc27cc6d177a
Rémi Robichet <remi.robichet@edtake.com>
2022-03-15 16:44:08 +0100

    wip protect routes

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,39 +1,44 @@
 <template>
   <div class="home">
     <div>
-      <input 
-        type="text" 
-        id="name" 
-        className="App-input" 
-        placeholder="Name" 
+      <input
+        type="text"
+        id="name"
+        className="App-input"
+        placeholder="Name"
         v-model="name"
-        />
+      />
       <button className="App-button" @click="getDataFromApi">Submit</button>
     </div>
-  <div><h5>Message: {{message}}</h5></div>
+    <div>
+      <h5>Message: {{ message }}</h5>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Form',
-  components: {
-  },
-  data () {
+  name: "Form",
+  components: {},
+  data() {
     return {
       name: undefined,
-      message: undefined
-    }
+      message: undefined,
+    };
   },
   methods: {
-    async getDataFromApi () {
-      const data = await fetch(`/api/hello?name=${this.name}`);
-      const json = await data.json();
-      if (json.message){
-        this.message = json.message
+    async getDataFromApi() {
+      try {
+        const data = await fetch(`/api/hello?name=${this.name}`);
+        const json = await data.json();
+        if (json.message) {
+          this.message = json.message;
+        }
+      } catch (err) {
+        console.log(`error:`, err);
+        this.message = "Error: you must be connected !";
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
