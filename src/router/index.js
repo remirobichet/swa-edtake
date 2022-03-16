e87cc98a29c95fac03551fc6a3eff408cdec528d
Rémi Robichet <remi.robichet@edtake.com>
2022-03-16 09:34:19 +0100

    wip

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import PageNotFound from '../views/PageNotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -13,16 +15,13 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: AboutView
+  },
+  { path: "*", component: PageNotFound }
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="err404">
+    404 ! 💥
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "PageNotFound",
+  components: {
+  }
+};
+</script>
